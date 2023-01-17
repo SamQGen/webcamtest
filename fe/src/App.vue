06ca7cf01f5f179b5dc3e99d41432d70ef8e8c05
sam <sanderson@qgengroup.com>
2023-01-17 12:25:30 +0100

    mediasoup attempt

--- fe/src/App.vue
+++ fe/src/App.vue
@@ -1,28 +1,65 @@
 <template>
   <video autoplay></video>
+  <button type="button" @click="clicked">Click Me!</button>
+  <textarea id="w3review" name="w3review" v-model="textArea">
+    dummyText
+</textarea>
 </template>
 
 <script setup>
-import {onMounted} from "vue";
+import { onMounted, ref } from 'vue';
+// import mediasoup from 'mediasoup'
+const socket = new WebSocket('ws://localhost:8080');
+const textArea = ref('')
+const clicked = () => {
+  socket.send(textArea.value)
+}
 
 onMounted(() => {
-  const socket = new WebSocket('wss://demo.piesocket.com/v3/channel_123?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self');
 
-  navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
-    const video = document.querySelector('video');
-    video.srcObject = stream;
-    video.onloadedmetadata = () => {
-      video.play();
-      console.log('this is the stream ' , stream)
-      const peer = new RTCPeerConnection();
-      peer.addStream(stream);
+  socket.onmessage = ({data}) => {
+    console.log(`message from server ` , data);
+  }
 
-      peer.ontrack = e => {
-        console.log('sending ' , e.streams[0])
-        socket.send(e.streams[0]);
-      };
-    };
-  });
+  navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+      .then((stream) => {
+        const videoTrack = stream.getVideoTracks()[0];
+        const peer = new mediasoup.Peer();
+        const transport = peer.createTransport({
+          kind: 'webrtc',
+          rtcConfiguration: {
+            iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
+          }
+        });
+        const producer = transport.produce({ track: videoTrack });
+        producer.on('track', (track) => {
+          console.log(`Sending video track over WebSocket ${track}`);
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+
+
+
+  // navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+  //   const video = document.querySelector('video');
+  //   video.srcObject = stream;
+  //   console.log('information about stream '  , stream.getVideoTracks());
+  //   video.onloadedmetadata = () => {
+  //     const videoTracks = stream.getVideoTracks()
+  //     // video.play();
+  //     // socket.send(stream)
+  //     console.log('this is the stream ' , stream)
+  //     const peer = new RTCPeerConnection();
+  //     peer.addTrack(videoTracks[0]);
+  //
+  //     peer.ontrack = e => {
+  //       console.log('sending ' , e.streams[0])
+  //       socket.send(e.streams[0]);
+  //     };
+  //   };
+  // });
 })
 </script>
 
