f5a0e68edcd42dc4639376249ed34c721b05d2d2
sam <sanderson@qgengroup.com>
2023-01-17 18:13:43 +0100

    be done

--- be/index.js
+++ be/index.js
@@ -1,10 +1,34 @@
 const WebSocket = require('ws')
+const fs = require('fs');
 const server = new WebSocket.Server({port:'8080'})
 let outerSocket = null;
+let timesRun =0;
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 server.on('connection' , socket => {
   socket.on('message',message =>{
-    console.log('got message ' , message.toString());
-    let interval = setInterval(()=> getSelPos(socket), 2000);
+    let content =JSON.parse(message.toString())
+    console.log('got message ' , JSON.parse(message.toString()));
+    console.log('this is the random number ' , content.random);
+    fs.writeFile('test.txt', content.data, err => {
+      if (err) {
+        console.error(err);
+      }
+      // file written successfully
+    });
+    let data = content.data.replace(/^data:image\/\w+;base64,/, "");
+    let buf = new Buffer(data, 'base64');
+    fs.writeFile('image.png', buf,err => {
+      console.log(err);});
+
+    // let interval = setInterval(()=> getSelPos(socket), 500);
+    // if (timesRun > 5){
+    //   clearInterval(interval)
+    // }
+    let randomint = {random:randomInteger(1,5)}
+
+    socket.send(JSON.stringify(randomint))
     // socket.send(`Recieved our message: ${message}`)
   })
 
@@ -15,6 +39,7 @@ server.on('connection' , socket => {
 })
 
 const getSelPos = async(ws) =>{
-  ws.send('dummy')
+  ws.send(`dummy ${timesRun}`)
+  timesRun +=1
   // Use socket instance
 }
