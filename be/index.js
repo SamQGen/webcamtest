1a531d5382100a3ad328657835f720f5c70736aa
sam <sanderson@qgengroup.com>
2023-01-17 15:05:46 +0100

    mock changes

--- be/index.js
+++ be/index.js
@@ -2,20 +2,19 @@ const WebSocket = require('ws')
 const server = new WebSocket.Server({port:'8080'})
 let outerSocket = null;
 server.on('connection' , socket => {
-  // socket.on('message',message =>{
-  //   console.log('got message ' , message.toString());
-  //   // socket.send(`Recieved our message: ${message}`)
-  // })
+  socket.on('message',message =>{
+    console.log('got message ' , message.toString());
+    let interval = setInterval(()=> getSelPos(socket), 2000);
+    // socket.send(`Recieved our message: ${message}`)
+  })
 
 
   socket.on('error', error => {
     console.log(`this is our error ${error}`);
   })
 })
-setTimeout(()=>{
-  server.on('connection', socket =>{
-    console.log('got our message' , socket);
-  })
-  // socket.send('dummy callback after 5 seconds')
-},3000)
 
+const getSelPos = async(ws) =>{
+  ws.send('dummy')
+  // Use socket instance
+}
