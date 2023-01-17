368f52b55e0363339e0031c1cfaff51513408796
sam <sanderson@qgengroup.com>
2023-01-17 18:13:52 +0100

    fe done

--- fe/src/App.vue
+++ fe/src/App.vue
@@ -1,30 +1,34 @@
 <template>
   <video autoplay></video>
   <button type="button" @click="clicked">Click Me!</button>
-  <img :src="imageEl" alt="" v-if="imageEl"/>
-  <textarea id="w3review" name="w3review" v-model="textArea">
-
-    dummyText
-</textarea>
+  <img :src="imageEl" alt="" v-if="false"/>
+  <textarea id="w3review" name="w3review" v-model="textArea" />
+  <h3>{{calculatedMatch}}</h3>
 </template>
 
 <script setup>
-import { onMounted, onUnmounted, ref, watch } from 'vue';
-// import mediasoup from 'mediasoup'
+import { computed, onMounted, onUnmounted, ref, watch } from 'vue';
 const socket = new WebSocket('ws://localhost:8080');
 let imageEl = ref('')
 let interval = ref('')
 const textArea = ref('')
-let count = 0;
+const randomIntFromServer = ref('')
+const generateRandom = () => {
+  return Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+}
+const randomIntLocal = generateRandom()
+const calculatedMatch = computed(()=>{
+  return randomIntFromServer.value === randomIntLocal ? true : false
+})
+
+
 const clicked = () => {
   socket.send(textArea.value)
 }
 
-watch(imageEl,  () => {
-  console.log('this is nv and ov ' );
-  count +=1
-  console.log(count);
-  socket.send(`${imageEl.value}`)
+watch(imageEl,  (nv) => {
+  let obj = {data:nv,test:'test',random:randomIntLocal}
+  socket.send(JSON.stringify(obj))
 })
 
 const getImage = (track) => {
@@ -39,51 +43,19 @@ const getImage = (track) => {
     };
   });
 }
-// let chunks = []
 onMounted(() => {
   socket.onmessage = ({data}) => {
-    console.log(`message from server ` , data);
+    randomIntFromServer.value=JSON.parse(data).random
+    console.log(`message from server ` , JSON.parse(data));
   }
   navigator.mediaDevices.getUserMedia({video: true})
       .then((mediaStream) => {
         document.querySelector('video').srcObject = mediaStream
         const track = mediaStream.getVideoTracks()[0];
+        // getImage(track)
         interval = setInterval(() => getImage(track),500)
-        // let imageCapture = new ImageCapture(track);
-        // imageCapture.takePhoto().then(img => {
-        //     let reader = new FileReader();
-        //     reader.readAsDataURL(img);
-        //     reader.onloadend = function () {
-        //       let base64data = reader.result;
-        //       imageEl.value = base64data;
-        //     };
-        // });
-
       })
       .catch((error) => console.error(error));
-    // navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
-    //   const video = document.querySelector('video');
-    //   video.srcObject = stream;
-    //   console.log('information about stream '  , stream.getVideoTracks());
-    //   const mediaRecorder = new MediaRecorder(stream);
-    //   mediaRecorder.start();
-    //   mediaRecorder.ondataavailable = (e) => {
-    //     console.log('this is the thing? ');
-    //     chunks.push(e.data);
-    //   };
-      // video.onloadedmetadata = () => {
-      //   const videoTracks = stream.getVideoTracks()
-      //   // video.play();
-      //   // socket.send(stream)
-      //   console.log('this is the stream ' , stream)
-      //   const peer = new RTCPeerConnection();
-      //   peer.addTrack(videoTracks[0]);
-      //
-      //   peer.ontrack = e => {
-      //     console.log('sending ' , e.streams[0])
-      //     socket.send(e.streams[0]);
-      //   };
-      // };
   onUnmounted(()=>{
     clearInterval(interval.value)
   })
